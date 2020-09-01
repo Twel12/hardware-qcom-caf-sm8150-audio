LineageOS/android_hardware_qcom_audio

hal: Add tfa98xx feedback extension
 * Based on observing reverse engineering from davinci stock audio HAL.
 * Fixes broken stereo sound.

Co-authored-by: Smosia <stasenko_nv@nrcki.ru>
Change-Id: Ia115d7be70f022468ad71ebb7d7aadda11c22c4c

hal/Android.mk
@@ -341,6 +341,11 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AHAL_EXT)),true)
     LOCAL_SHARED_LIBRARIES += vendor.qti.hardware.audiohalext@1.0
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLE_TFA98XX_FEEDBACK)),true)
+    LOCAL_CFLAGS += -DENABLE_TFA98XX_FEEDBACK
+    LOCAL_SRC_FILES += audio_extn/tfa98xx_feedback.c
+endif
+
 LOCAL_CFLAGS += -D_GNU_SOURCE
 LOCAL_CFLAGS += -Wall -Werror
 

Twel12/hardware-qcom-caf-sm8150-audio

hal/Android.mk
@@ -341,6 +341,11 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AHAL_EXT)),true)
     LOCAL_SHARED_LIBRARIES += vendor.qti.hardware.audiohalext@1.0
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_TFA98XX_FEEDBACK)),true)
+    LOCAL_CFLAGS += -DENABLE_TFA98XX_FEEDBACK
+    LOCAL_SRC_FILES += audio_extn/tfa98xx_feedback.c
+endif
+
 LOCAL_CFLAGS += -D_GNU_SOURCE
 LOCAL_CFLAGS += -Wall -Werror
 